580f91b17d5bb2f6fc12799a7549ff463567cd25
Mufasa007 <594564264@qq.com>
2019-03-22 22:12:03 +0800

    单辆车的dijstra最短路径

--- function/priority_queue.py
+++ function/priority_queue.py
@@ -1,21 +1,32 @@
 # 自建优先队列
 # data是一个有序数组，d0是一个需要插入的数值
-def priority_queue(data, d0):
-    index = int(len(data) / 2)
-    while 1:
-        if data[index] == d0:
-            data.insert(index,d0)
+def priority_queue(data, d0):   # 二分查找法的优先队列建立
+    if d0 < data[0]:
+        data.insert(0, d0)
+        return data
+    elif d0 > data[-1]:
+        data.append(d0)
+        return data
+
+    low = 0
+    high = len(data) - 1
+    while (high - low) > 1:
+        mid = low + int((high - low) / 2)
+        # print(low, high)
+        if data[mid] == d0:
+            data.insert(mid, d0)
             return data
-        elif data[index] < d0:
-
-        elif data[index] > d0:
-
-
+        elif data[mid] < d0:
+            low = mid
+        elif data[mid] > d0:
+            high = mid
+    data.insert(mid, d0)
     return data
 
 
 if __name__ == "__main__":
-    data = [1, 2, 3, 5, 6, 7]
-    d0 = 4
+    data = [1, 2, 3, 6, 6, 7]
+    # d0 = 4
+    d0 = 5
     d1 = priority_queue(data, d0)
     print(d1)
--- function/测试.py
+++ function/测试.py
@@ -1,5 +1,21 @@
-import os
-os.chdir(os.getcwd())
-print(os.getcwd())
-print(os.path.dirname(os.path.realpath(__file__)))
-print(os.path.realpath(__file__))
+import numpy as np
+
+
+
+# print(np.eye(10, k=1))
+print(np.eye(10))
+
+
+# d0 = [1,[3,4,5]]
+# d1 = list(d0)
+# print(d1)
+
+#
+# d0 = np.zeros((4,4))
+# print(d0)
+
+# import os
+# os.chdir(os.getcwd())
+# print(os.getcwd())
+# print(os.path.dirname(os.path.realpath(__file__)))
+# print(os.path.realpath(__file__))
