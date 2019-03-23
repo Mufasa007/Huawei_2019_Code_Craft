efd4d6d6e311c79be8cc3f8f12be80b684d4bd1f
Mufasa007 <594564264@qq.com>
2019-03-23 16:31:55 +0800

    编写评价程序

---
+++ evaluation/road_running.py
@@ -0,0 +1,17 @@
+# 目标：输入road、car、cross原始数据&car_path，输出道路上车辆信息，以及时间状态信息
+# 采用分治法来进行问题的解决
+# 功能块：
+# 1，十字路口的节点判断；
+# 2，井字map的逻辑判断
+
+class road_logic:
+    def __init__(self, road, cross, car, car_path): # 原始数据输入
+        self.road = road
+        self.cross = cross
+        self.car = car
+        self.car_path = car_path
+
+    def cross_logic(self,cross_values): # 十字路口的运行逻辑
+
+    def
+        
--- function/测试.py
+++ function/测试.py
@@ -1,16 +1,17 @@
 import numpy as np
 
 
+d0 = {1:{2:3}}
+print(d0)
+
 
 # print(np.eye(10, k=1))
-print(np.eye(10))
-
+# print(np.eye(10))
 
 # d0 = [1,[3,4,5]]
 # d1 = list(d0)
 # print(d1)
 
-#
 # d0 = np.zeros((4,4))
 # print(d0)
 
